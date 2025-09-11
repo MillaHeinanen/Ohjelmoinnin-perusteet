093aa109efbb11c8c59f55fb68da524b6f54e98d
MillaHeinanen <milla.heinanen@student.lab.fi>
2025-09-11 14:31:56 +0300

    kaikki

---
+++ Cheat code.py
@@ -0,0 +1,25 @@
+print("Hello World!")
+print("Here we are")
+print("He said: \"Hello\", and kept walking")
+print("He said hello \n \r \t \b and \kept walking")
+
+exampleString="Text and numb3rs"
+exampleInt = 12
+exampleFloat = 12.12
+exampleBoolean = False
+
+#Firstname=input("What is your firstname: ")
+#Lastname=input("What is your lastname: ")
+#print("Hello", Firstname, Lastname)
+
+Sentence="Finding substring"
+print(Sentence)
+print(Sentence[0])
+print(Sentence[-2])
+print(Sentence[2:9])
+print(Sentence[:9])
+print(Sentence[-9:-2])
+print(Sentence[2:9:3])
+print(Sentence[::3])
+print(Sentence[::-1])
+
