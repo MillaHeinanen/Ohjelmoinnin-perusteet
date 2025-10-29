f9d3264693c81bbe7bd48cd840dc57a5b9055845
MillaHeinanen <milla.heinanen@student.lab.fi>
2025-10-29 13:31:18 +0200

    commit

--- TASKS_W5/A5_T5.py
+++ TASKS_W5/A5_T5.py
@@ -1,31 +1,3 @@
-# def optionMenu():
-#     print("Options:")
-#     print("1 - Insert word")
-#     print("2 - Show current word")
-#     print("3 - Show current word in reverse")
-#     print("0 - Exit")
-#     return int(input("Your choice: "))
-
-# def main():
-#     print("Program starting.")
-#     word = ""
-#     choice = -1
-#     while choice != 0:
-#         choice = optionMenu()
-#         if choice == 1:
-#             word = input("Insert word: ")
-#         elif choice == 2:
-#             print(f"Current word - \"{word}\"")
-#         elif choice == 3:
-#             print(f"Word reversed - \"{word[::-1]}\"")
-#         elif choice == 0:
-#             print("Exiting program.")
-#             print("\nProgram ending.")
-#             break
-#         else:
-#              print("Unknown option! Try again")
-    
-# main()
 def optionMenu() -> int:
     print("\nOptions:")
     print("1 - Insert word")
